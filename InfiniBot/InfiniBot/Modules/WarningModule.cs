91d22af4128a8ea54d641edeecbe69775a71c1c0
Greedforce <emil.olsson@hotmail.com>
2020-06-14 19:45:38 +0200

    WarningModule.cs created, basis for command implemented.

---
+++ InfiniBot/InfiniBot/Modules/WarningModule.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace InfiniBot
+{
+    public class WarningModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("Warn", RunMode = RunMode.Async)]
+        [Alias("Warning", "Strike")]
+        [Summary("Warns the user a list of all available roles")]
+        public async Task WarnUserAsync(
+            [Summary("The user who will receive the warning.")]
+            [Example("@InfiniBot#6309")]
+            [Example("238279899687813120")]
+            SocketUser user,
+            [Summary("The reason for the warning.")]
+            [Example("Posted racist and/or hopophobic content.")]
+            string reason)
+        {
+
+        }
+    }
+}
